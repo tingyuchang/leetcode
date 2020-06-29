ca978159d09d28bee55c62d8b2c902e9a49b9d75
tingyuchang <tingyugenius@gmail.com>
2020-06-29 17:45:23 +0800

    fotmatted

--- maxArea/maxArea.go
+++ maxArea/maxArea.go
@@ -1,1 +1,29 @@
 package maxArea
+
+func MaxArea(height []int) int {
+	var area int
+
+	for i, v := range height {
+		for j := i + 1; j < len(height); j++ {
+			k := height[j]
+			tempArea := (j - i) * min(v, k)
+			area = max(area, tempArea)
+		}
+	}
+
+	return area
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+func min(i, j int) int {
+	if i > j {
+		return j
+	}
+	return i
+}
