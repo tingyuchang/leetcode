a4784b38a45a3296fecf164d68a829fce570475a
tingyuchang <tingyugenius@gmail.com>
2020-07-01 10:55:58 +0800

    seems cleverly, but not faster XD
    Runtime: 12 ms, faster than 43.30% of Go online submissions for Integer to Roman.
    Memory Usage: 4.3 MB, less than 15.00% of Go online submissions for Integer to Roman.

--- intToRoman/intToRoman.go
+++ intToRoman/intToRoman.go
@@ -3,80 +3,17 @@ package intToRoman
 import "fmt"
 
 func IntToRoman(num int) string {
-	thousand := num/1000
-	hundred :=  (num%1000)/100
-	ten := (num%100)/10
-	zero := num%10
+	value := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 
 	var roman string
 
-	for i := 0; i < thousand; i++ {
-		roman = fmt.Sprintf("%s%s", roman, "M")
+	for i :=0; i < len(value) && num >= 0; i++ {
+		for num >= value[i] {
+			num -= value[i]
+			roman = fmt.Sprintf("%s%v", roman, symbols[i])
+		}
 	}
-
-	switch hundred {
-		case 1:
-			roman = fmt.Sprintf("%s%s", roman, "C")
-		case 2:
-			roman = fmt.Sprintf("%s%s", roman, "CC")
-		case 3:
-			roman = fmt.Sprintf("%s%s", roman, "CCC")
-		case 4:
-			roman = fmt.Sprintf("%s%s", roman, "CD")
-		case 5:
-			roman = fmt.Sprintf("%s%s", roman, "D")
-		case 6:
-			roman = fmt.Sprintf("%s%s", roman, "DC")
-		case 7:
-			roman = fmt.Sprintf("%s%s", roman, "DCC")
-		case 8:
-			roman = fmt.Sprintf("%s%s", roman, "DCCC")
-		case 9:
-			roman = fmt.Sprintf("%s%s", roman, "CM")
-	}
-
-	switch ten {
-		case 1:
-			roman = fmt.Sprintf("%s%s", roman, "X")
-		case 2:
-			roman = fmt.Sprintf("%s%s", roman, "XX")
-		case 3:
-			roman = fmt.Sprintf("%s%s", roman, "XXX")
-		case 4:
-			roman = fmt.Sprintf("%s%s", roman, "XL")
-		case 5:
-			roman = fmt.Sprintf("%s%s", roman, "L")
-		case 6:
-			roman = fmt.Sprintf("%s%s", roman, "LX")
-		case 7:
-			roman = fmt.Sprintf("%s%s", roman, "LXX")
-		case 8:
-			roman = fmt.Sprintf("%s%s", roman, "LXXX")
-		case 9:
-			roman = fmt.Sprintf("%s%s", roman, "XC")
-	}
-
-	switch zero {
-		case 1:
-			roman = fmt.Sprintf("%s%s", roman, "I")
-		case 2:
-			roman = fmt.Sprintf("%s%s", roman, "II")
-		case 3:
-			roman = fmt.Sprintf("%s%s", roman, "III")
-		case 4:
-			roman = fmt.Sprintf("%s%s", roman, "IV")
-		case 5:
-			roman = fmt.Sprintf("%s%s", roman, "V")
-		case 6:
-			roman = fmt.Sprintf("%s%s", roman, "VI")
-		case 7:
-			roman = fmt.Sprintf("%s%s", roman, "VII")
-		case 8:
-			roman = fmt.Sprintf("%s%s", roman, "VIII")
-		case 9:
-			roman = fmt.Sprintf("%s%s", roman, "IX")
-	}
-
 
 	return roman
 }
