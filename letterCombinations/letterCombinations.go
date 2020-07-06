707b64384f98e1b324e2a23929153225dcac1daa
tingyuchang <tingyugenius@gmail.com>
2020-07-06 17:13:33 +0800

    iterative solution
    Runtime: 0 ms, faster than 100.00% of Go online submissions for Letter Combinations of a Phone Number.
    Memory Usage: 2.1 MB, less than 24.88% of Go online submissions for Letter Combinations of a Phone Number.

--- letterCombinations/letterCombinations.go
+++ letterCombinations/letterCombinations.go
@@ -6,6 +6,10 @@ var result []string
 var template map[string][]string
 func LetterCombinations(digits string) []string {
 
+	if len(digits) == 0 {
+		return []string{}
+	}
+
 	template = make(map[string][]string)
 	template["2"] = []string{"a", "b", "c"}
 	template["3"] = []string{"d", "e", "f"}
@@ -23,20 +27,40 @@ func LetterCombinations(digits string) []string {
 	}
 
 	result = []string{}
-	backTrack("", nums)
+	for _,v := range template[nums[0]]{
+		combination := v
+		result = append(result, combination)
+	}
+
+
+	for i := 1; i< len(nums); i++ {
+		// 3, 4, 5, 6...etc
+		options := template[nums[i]]
+		temp := []string{}
+		for j :=0; j < len(result); j++ {
+			// a,b ,c
+			for k := 0; k <len(options); k++ {
+				combination := fmt.Sprintf("%v%v", result[j], options[k])
+				temp = append(temp, combination)
+			}
+		}
+		result = temp
+	}
+
+
 	return result
 }
 
-func backTrack (combination string, nextDigit []string) {
-	if len(nextDigit) == 0 {
-		if len(combination) > 0 {
-			result = append(result, combination)
-		}
-	} else {
-		for _,v := range template[nextDigit[0]] {
-			backTrack(fmt.Sprintf("%v%v", combination, v), nextDigit[1:])
-		}
-	}
-}
+//func backTrack (combination string, nextDigit []string) {
+//	if len(nextDigit) == 0 {
+//		if len(combination) > 0 {
+//			result = append(result, combination)
+//		}
+//	} else {
+//		for _,v := range template[nextDigit[0]] {
+//			backTrack(fmt.Sprintf("%v%v", combination, v), nextDigit[1:])
+//		}
+//	}
+//}
 
 
