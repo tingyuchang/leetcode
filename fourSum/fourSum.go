b56008ffeadda5a83eee5655d6502a4fd4ff5534
tingyuchang <tingyugenius@gmail.com>
2020-07-07 13:04:11 +0800

    Recusive solution for K-Sum basic func is twoSum, dynamic to find target
    Runtime: 4 ms, faster than 98.05% of Go online submissions for 4Sum.
    Memory Usage: 2.9 MB, less than 39.24% of Go online submissions for 4Sum.

--- fourSum/fourSum.go
+++ fourSum/fourSum.go
@@ -1,57 +1,56 @@
 package fourSum
 
 import (
+	"fmt"
 	"sort"
 )
 
 func FourSum(nums []int, target int) [][]int {
-	var result [][]int
 	sort.Ints(nums)
+	return kSum(nums, target, 4)
+}
+
+func kSum(nums []int, target, k int) [][]int {
+	var results [][]int
+
+	if len(nums) == 0 || nums[0] *k > target || target > nums[len(nums)-1]*k {
+		return results
+	}
+
+	if k == 2 {
+		return twoSum(nums, target)
+	}
 
 	for i,v := range nums {
-		if i > 0 && v == nums[i-1] {
-			continue
-		}
-
-		temp := threeSum(nums[i+1:], target-v)
-		if len(temp) > 0 {
-			for _,s := range temp {
-				s = append(s, v)
-				result = append(result, s)
+		fmt.Println(v)
+		if i == 0 || nums[i] != nums[i-1] {
+			for _,v2 := range kSum(nums[i+1:], target-nums[i], k-1) {
+				v2 = append(v2, v)
+				results = append(results,v2)
 			}
 		}
 	}
 
-	return  result
+	return results
 }
 
-func threeSum(nums []int, target int) [][]int {
+
+func twoSum(nums []int, target int) [][]int {
 	var results [][]int
+	l := 0
+	r := len(nums)-1
 
-
-	for i,v := range nums {
-		if i > 0 && v == nums[i-1] {
-			continue
+	for l < r {
+		sum := nums[l] + nums[r]
+		if sum > target || (r < len(nums)-1 && nums[r] == nums[r+1]){
+			r--
+		} else if sum < target || (l > 0 && nums[l] == nums[l-1]){
+			l++
+		} else {
+			results = append(results, []int{nums[l], nums[r]})
+			l++
+			r--
 		}
-
-		l := i+1
-		r := len(nums)-1
-
-		for l < r {
-			sum := v + nums[l] + nums[r]
-			if sum > target {
-				r--
-			} else if sum < target {
-				l++
-			} else {
-				results = append(results, []int{v, nums[l], nums[r]})
-				l++
-				for nums[l] == nums[l-1] && l < r {
-					l++
-				}
-			}
-		}
-
 	}
 
 	return results
--- test/fourSum_test.go
+++ test/fourSum_test.go
@@ -17,15 +17,23 @@ func TestFourSum(t *testing.T) {
 			0,
 			[][]int{[]int{-1, 0, 0, -1}, []int{-2, -1, 1, 2}, []int{-2, 0, 0, 2}},
 		},
+		{
+			[]int{-3, -1, 0, 2, 4, 5},
+			2,
+			[][]int{[]int{-3, -1, 2, 4}},
+		},
 	}
 
 	for _,td := range testData {
 		result := fourSum.FourSum(td.input, td.target)
-		for i,v := range result {
-			sort.Ints(result[i])
-			sort.Ints(v)
-			assert.Equal(t, result[i], v)
+		assert.Equal(t, len(result), len(td.expected))
+
+		if len(result) == len(td.expected) {
+			for i,v := range td.expected {
+				sort.Ints(v)
+				sort.Ints(result[i])
+				assert.Equal(t, result[i], v)
+			}
 		}
 	}
-
 }
