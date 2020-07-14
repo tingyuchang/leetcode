9ddf2bc2dd442aaf5dce99f64892aec94f7af5fa
tingyuchang <tingyugenius@gmail.com>
2020-07-14 17:00:07 +0800

    Update Brute Forec: Runtime: 12 ms, faster than 85.92% of Go online submissions for Median of Two Sorted Arrays. Memory Usage: 5.7 MB, less than 51.30% of Go online submissions for Median of Two Sorted Arrays.

--- findMedianSortedArrays/findMedianSortedArrays.go
+++ findMedianSortedArrays/findMedianSortedArrays.go
@@ -1,83 +1,44 @@
 package findMedianSortedArrays
 
 func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	m := len(nums1)
-	n := len(nums2)
+	m, n := len(nums1), len(nums2)
+	l :=  m+ n
 
-	if  m > n {
-		nums1, nums2 = nums2, nums1
-		m, n = n, m
+	index1 := 0
+	index2 := 0
+	start := 0
+	var temp [2]int
+
+	for start <= l/2 && index1 < m && index2 < n {
+		if nums1[index1] < nums2[index2] {
+			temp[0], temp[1] = temp[1], nums1[index1]
+			index1++
+		} else {
+			temp[0], temp[1] = temp[1], nums2[index2]
+			index2++
+		}
+		start++
 	}
 
-	iMin, iMax, half := 0, m, (m+n+1)/2
-
-	for iMin <= iMax {
-		i := (iMax+iMin)/2
-		j := half - i
-
-		if i < m && nums2[j-1] > nums1[i] {
-			iMin = i + 1
-		} else if i > 0 && nums1[i-1] > nums2[j] {
-			iMax = i - 1
+	if start <= l/2 && index1 < m {
+		if l/2 - start >=1 {
+			temp[0], temp[1] = nums1[index1+l/2-start-1], nums1[index1+l/2-start]
 		} else {
-			// correct i
-			var maxLeft int
-			var minRight int
-			if i == 0 {
-				maxLeft = nums2[j-1]
-			} else if j == 0 {
-				maxLeft = nums1[i-1]
-			} else  {
-				maxLeft = max(nums1[i-1], nums2[j-1])
-			}
-
-			if (m+n)%2 == 1 {
-				return float64(maxLeft)
-			}
-
-			if i == m {
-				minRight = nums2[j]
-			} else if j == n {
-				minRight = nums1[i]
-			} else {
-				minRight = min(nums1[i], nums2[j])
-			}
-			return (float64(maxLeft)+float64(minRight))/2.0
+			temp[0], temp[1] = temp[1], nums1[index1]
 		}
 	}
 
-	return 0.0
-}
-
-func max (i, j int) int {
-	if i > j {
-		return i
+	if start <= l/2 && index2 < n  {
+		if l/2 - start >=1 {
+			temp[0], temp[1] = nums2[index2+l/2-start-1], nums2[index2+l/2-start]
+		} else {
+			temp[0], temp[1] = temp[1], nums2[index2]
+		}
 	}
 
-	return j
-}
-
-func min (i, j int) int {
-	if i > j {
-		return j
+	if l%2 == 0 {
+		return float64(temp[len(temp)-1] + temp[len(temp)-2])/2.0
 	}
 
-	return i
+	return float64(temp[len(temp)-1])
 }
-/*
-
-  # i is perfect
-
-            if i == 0: max_of_left = B[j-1]
-            elif j == 0: max_of_left = A[i-1]
-            else: max_of_left = max(A[i-1], B[j-1])
-
-            if (m + n) % 2 == 1:
-                return max_of_left
-
-            if i == m: min_of_right = B[j]
-            elif j == n: min_of_right = A[i]
-            else: min_of_right = min(A[i], B[j])
-
-            return (max_of_left + min_of_right) / 2.0
- */
