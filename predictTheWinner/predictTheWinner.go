1f3dbc7a4439614b0df00febc13990c6abdca5f6
tingyuchang <tingyugenius@gmail.com>
2020-07-21 11:00:37 +0800

    temp commit, not resolve

--- predictTheWinner/predictTheWinner.go
+++ predictTheWinner/predictTheWinner.go
@@ -1,5 +1,23 @@
 package predictTheWinner
 
 func PredictTheWinner(nums []int) bool {
+	return winner(nums, 0, len(nums)-1, 1) >=0
+}
 
+func winner(si []int, start, end, turn int ) int {
+	if start == end {
+		return turn * si[start]
+	}
+
+	a := turn * si[start] + winner(si, start+1, end, -turn)
+	b := turn * si[end] + winner(si, start, end-1, -turn)
+
+	return turn * max(turn*a, turn*b)
+}
+
+func max(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
 }
