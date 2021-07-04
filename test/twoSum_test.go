3acf146316a8f3737f816f956c70141faeb366d3
tingyuchang <tingyugenius@gmail.com>
2021-07-04 20:36:56 +0800

    Brute force:
    Runtime: 28 ms, faster than 24.55% of Go online submissions for Two Sum.
    Memory Usage: 3.7 MB, less than 71.60% of Go online submissions for Two Sum.

---
+++ test/twoSum_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"github.com/magiconair/properties/assert"
+	"leetcode/twoSum"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	testData := []struct{
+		input []int
+		target int
+		expected []int
+	} {
+		{[]int{2,7,11,15}, 9, []int{0,1}},
+		{[]int{3,2,4}, 6, []int{1,2}},
+		{[]int{3,3}, 6, []int{0,1}},
+	}
+
+	for _,tt := range testData {
+		result := twoSum.TwoSum(tt.input, tt.target)
+
+		assert.Equal(t, result, tt.expected)
+	}
+}
