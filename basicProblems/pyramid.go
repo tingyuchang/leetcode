72e5a0b71c7efd0014ad665ceff23439482c85f2
tingyuchang <tingyugenius@gmail.com>
2021-07-11 12:54:40 +0800

    fixed pyramid using error pyramid count, should be 2*n-1, not pow(2, n)-1

--- basicProblems/pyramid.go
+++ basicProblems/pyramid.go
@@ -2,7 +2,6 @@ package basicProblems
 
 import (
 	"fmt"
-	"math"
 )
 
 /*
@@ -22,23 +21,12 @@ pyramid has spaces on both the left *and* right hand sides
 */
 func pyramid(n int) []string {
 	var results []string
-	var pyramidNum int
-
-	if n == 1 {
-		pyramidNum = 1
-	} else {
-		pyramidNum = int(math.Pow(2, float64(n-1)))+1
-	}
+	pyramidNum := 2*n-1
 	for i:=0;i<n;i++{
 		tmpStr := ""
 
 		for j:=0;j<pyramidNum;j++{
-			current :=0
-			if i == 0 {
-				current = 1
-			} else {
-				current = int(math.Pow(2, float64(i)))+1
-			}
+			current := 2*(i+1)-1
 			if j < (pyramidNum-current)/2 || j >= (pyramidNum+current)/2   {
 				tmpStr+=" "
 			} else {
