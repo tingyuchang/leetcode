e52027c7ddf1dcb12e26bb2743e87f977c9d3f44
tingyuchang <tingyugenius@gmail.com>
2021-07-16 12:00:26 +0800

    insert auto generated testing code by using Goland helper (looks good)

--- basicProblems/anagrams_test.go
+++ basicProblems/anagrams_test.go
@@ -58,3 +58,24 @@ func BenchmarkAnagram2(b *testing.B) {
 		anagrams("hello", "lloheaa")
 	}
 }
+
+func Test_anagrams2(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"basic test", args{"hello", "llohe"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := anagrams2(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("anagrams2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
