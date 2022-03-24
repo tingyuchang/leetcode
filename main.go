88f5608247dbaec740314addf5710796236769af
tingyuchang <tingyugenius@gmail.com>
2022-03-24 15:19:14 +0800

    add: tree & binary tree

--- main.go
+++ main.go
@@ -2,12 +2,61 @@ package main
 
 import (
 	"fmt"
-	"leetcode/divide"
+	"leetcode/basicProblems/tree"
 	"leetcode/mergeKLists"
 )
 
 func main() {
-	fmt.Println("result:", divide.Divide(720, 2))
+	node1 := &tree.Node{
+		Value: 20,
+		Children: nil,
+	}
+
+	node2 := &tree.Node{
+		Value: 0,
+		Children: []*tree.Node{
+			&tree.Node{
+				Value:12,
+			},
+			&tree.Node{
+				Value:-2,
+			},
+			&tree.Node{
+				Value:1,
+			},
+		},
+	}
+	node3 := &tree.Node{
+		Value: 40,
+		Children: nil,
+	}
+	node4 := &tree.Node{
+		Value: 15,
+		Children: []*tree.Node{
+			&tree.Node{
+				Value:-2,
+			},
+		},
+	}
+
+	node1.Children = append(node1.Children, node2, node3, node4)
+
+	tree1 := tree.Tree{
+		Root: node1,
+	}
+
+	for _,v := range tree1.BreadthFirst() {
+		fmt.Printf("%v ", v.Value)
+	}
+
+	fmt.Println("")
+
+	for _,v := range tree1.DepthFirst() {
+		fmt.Printf("%v ", v.Value)
+	}
+
+
+
 }
 
 func generateList(firstNodes[]int) (*mergeKLists.ListNode) {
