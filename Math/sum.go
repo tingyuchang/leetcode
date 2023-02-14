870fb3551cf183c0ccdddccc2e16d35f2dd5bac6
Matt Chang <tingyugenius@gmail.com>
2023-02-14 15:13:10 +0800

    ADD: finished 3sum

--- Math/sum.go
+++ Math/sum.go
@@ -1,38 +1,42 @@
 package Math
 
+import (
+	"sort"
+)
+
 func ThreeSum(nums []int) [][]int {
 	result := make([][]int, 0)
+	sort.Ints(nums)
 
 	for i := 0; i < len(nums); i++ {
-		others := make([]int, 0)
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
 
-		for j := 0; j < len(nums); j++ {
-			if j == i {
-				continue
+		l := i + 1
+		r := len(nums) - 1
+
+		for r > l {
+			sum := nums[i] + nums[l] + nums[r]
+
+			if sum > 0 {
+				r--
+			} else if sum < 0 {
+				l++
+			} else {
+				// get it
+				result = append(result, []int{nums[i], nums[l], nums[r]})
+				l++
+				// skip same value in l
+				for l < r && nums[l] == nums[l-1] {
+					l++
+				}
 			}
-			others = append(others, nums[j])
 		}
-		temp := TwoSumV2(others, -nums[i])
 
-		if len(temp) == 2 {
-			result = append(result, []int{nums[i], temp[0], temp[1]})
-		}
 	}
+
 	return result
-}
-
-func TwoSumV2(nums []int, target int) []int {
-	m := make(map[int]int)
-	for i, v := range nums {
-		_, ok := m[v]
-
-		if ok {
-			return []int{nums[m[v]], nums[i]}
-		} else {
-			m[target-v] = i
-		}
-	}
-	return nil
 }
 
 func TwoSum(nums []int, target int) []int {
