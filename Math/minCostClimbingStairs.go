8cd626a66dbd0379b0d7d3ccc776537c49f2f7d6
Matt Chang <tingyugenius@gmail.com>
2023-02-14 11:43:07 +0800

    ADD: 746. Min Cost Climbing Stairs

---
+++ Math/minCostClimbingStairs.go
@@ -0,0 +1,18 @@
+package Math
+
+func MinCostClimbingStairs(cost []int) int {
+	cost = append(cost, 0)
+
+	for i := len(cost) - 3; i >= 0; i-- {
+		cost[i] = cost[i] + min(cost[i+1], cost[i+2])
+	}
+
+	return min(cost[0], cost[1])
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
