ef3628b789935904237a8fda3b89dffd79058832
Matt Chang <tingyugenius@gmail.com>
2023-02-20 10:24:25 +0800

    ADD: 112. Path Sum

---
+++ Tree/path.go
@@ -0,0 +1,13 @@
+package Tree
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Val == targetSum && root.Left == nil && root.Right == nil {
+		return true
+	}
+	
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
