66bae0241cd4ced527fc469878e06d455600537f
Matt Chang <tingyugenius@gmail.com>
2023-02-22 14:17:42 +0800

    ADD: 200. Number of Islands

---
+++ test/bfs_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"github.com/magiconair/properties/assert"
+	"leetcode/BFS"
+	"testing"
+)
+
+func TestNumsOfIslands(t *testing.T) {
+	testData := []struct {
+		input    [][]byte
+		expected int
+	}{
+		{[][]byte{
+			{'1', '1', '1', '1', '0'},
+			{'1', '1', '0', '1', '0'},
+			{'1', '1', '0', '0', '0'},
+			{'0', '0', '0', '0', '0'},
+		}, 1},
+		{[][]byte{
+			{'1', '1', '0', '0', '0'},
+			{'1', '1', '0', '0', '0'},
+			{'0', '0', '1', '0', '0'},
+			{'0', '0', '0', '1', '1'},
+		}, 3},
+		{[][]byte{{'0'}}, 0},
+	}
+
+	for _, td := range testData {
+		result := BFS.NumIslands(td.input)
+		assert.Equal(t, result, td.expected)
+	}
+}
