6a204cfb1e43c3ec821616c531b124359f2aaef2
Matt Chang <tingyugenius@gmail.com>
2023-03-01 10:15:31 +0800

    ADD: 326 Power of Three

--- Math/power.go
+++ Math/power.go
@@ -14,3 +14,25 @@ func IsPowerOfTwo(n int) bool {
 
 	return true
 }
+
+func isPowerOfThree(n int) bool {
+	if n <= 0 {
+		return false
+	}
+
+	for n > 1 {
+		if n%3 != 0 {
+			return false
+		}
+
+		n = n / 3
+	}
+
+	/*
+		a :=1
+		for a < 3 { a*=3 }
+		return a == n
+	*/
+
+	return true
+}
