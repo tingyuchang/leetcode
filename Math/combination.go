fe2e7fa199772700d5233e3a7806ae5e6db47b32
tingyuchang <tingyugenius@gmail.com>
2023-03-04 14:38:21 +0800

    ADD: 2444. Count Subarrays With Fixed Bounds

--- Math/combination.go
+++ Math/combination.go
@@ -1,6 +1,49 @@
 package Math
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
+
+func CountSubarrays(nums []int, minK int, maxK int) int64 {
+	var res int64
+	l := -1
+	lastMin, lastMax := -1, -1
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+
+	for i := 0; i < len(nums); i++ {
+
+		if nums[i] >= minK && nums[i] <= maxK {
+			if nums[i] == minK {
+				lastMin = i
+			}
+			if nums[i] == maxK {
+				lastMax = i
+			}
+
+			res += int64(max(0, min(lastMin, lastMax)-l))
+			fmt.Println(res)
+		} else {
+			l = i
+			lastMin, lastMax = -1, -1
+		}
+	}
+
+	return res
+}
 
 func CombinationSum(candidates []int, target int) [][]int {
 	sort.Ints(candidates)
