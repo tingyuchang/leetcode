bc368897e67de42f489b5aaa9a5d910ff880fb6d
Matt Chang <tingyugenius@gmail.com>
2023-03-07 11:16:41 +0800

    ADD: 435. Non-overlapping Intervals

--- greedy/eraseOverlapIntervals.go
+++ greedy/eraseOverlapIntervals.go
@@ -1,1 +1,24 @@
 package greedy
+
+import (
+	"math"
+	"sort"
+)
+
+func EraseOverlapIntervals(intervals [][]int) int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][1] < intervals[j][1]
+	})
+
+	count := 0
+	end := math.MinInt
+	for _, interval := range intervals {
+		if interval[0] >= end {
+			end = interval[1]
+		} else {
+			count++
+		}
+	}
+
+	return count
+}
--- test/greedy_test.go
+++ test/greedy_test.go
@@ -1,1 +1,26 @@
 package test
+
+import (
+	"github.com/magiconair/properties/assert"
+	"leetcode/greedy"
+	"testing"
+)
+
+func TestEraseOverlapIntervals(t *testing.T) {
+	testData := []struct {
+		intervals [][]int
+		expected  int
+	}{
+		{
+			[][]int{
+				{1, 2}, {2, 3}, {3, 4}, {1, 3},
+			},
+			1,
+		},
+	}
+
+	for _, td := range testData {
+		result := greedy.EraseOverlapIntervals(td.intervals)
+		assert.Equal(t, result, td.expected)
+	}
+}
