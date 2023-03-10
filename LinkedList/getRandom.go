943c1d677664bbd991816a675beccb01625ebe2d
tingyuchang <tingyugenius@gmail.com>
2023-03-10 12:00:00 +0800

    ADD: 382. Linked List Random Node

--- LinkedList/getRandom.go
+++ LinkedList/getRandom.go
@@ -1,14 +1,27 @@
 package LinkedList
 
+import "math/rand"
+
 type Solution struct {
+	lists []*ListNode
 }
 
 func Constructor(head *ListNode) Solution {
-
+	solution := Solution{lists: make([]*ListNode, 0)}
+	node := head
+	for node != nil {
+		solution.lists = append(solution.lists, node)
+		node = node.Next
+	}
+	return solution
 }
 
 func (this *Solution) GetRandom() int {
+	return this.lists[rand.Intn(this.Len())-1].Val
+}
 
+func (this *Solution) Len() int {
+	return len(this.lists)
 }
 
 /**
