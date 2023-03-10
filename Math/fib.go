072745e8f81eb7bd83f979260ae3a706ce8ce290
tingyuchang <tingyugenius@gmail.com>
2023-03-10 16:48:27 +0900

    ADD: 509. Fibonacci Number

--- Math/fib.go
+++ Math/fib.go
@@ -1,11 +1,31 @@
 package Math
 
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 {
+		return 1
+	}
+
+	temp1 := 0
+	temp2 := 1
+	for i := 2; i <= n; i++ {
+		temp3 := temp1 + temp2
+		temp1, temp2 = temp2, temp3
+	}
+
+	return temp2
+}
+
 func climbStairs(n int) int {
 	if n <= 1 {
 		return 1
 	}
 
 	temp1 := 1
+
 	temp2 := 1
 
 	for i := 2; i <= n; i++ {
