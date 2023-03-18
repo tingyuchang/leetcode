6d3ef5982dda82cf4ee49281e04f266409897f2f
tingyuchang <tingyugenius@gmail.com>
2023-03-18 21:49:10 +0800

    ADD: 1472 Design Browser History

--- data-structure/BrowserHistory/model.go
+++ data-structure/BrowserHistory/model.go
@@ -1,22 +1,43 @@
 package BrowserHistory
 
 type BrowserHistory struct {
+	history []string
+	current int
 }
 
 func Constructor(homepage string) BrowserHistory {
-
+	return BrowserHistory{[]string{homepage}, 0}
 }
 
 func (this *BrowserHistory) Visit(url string) {
+	if this.current == len(this.history)-1 {
+		this.history = append(this.history, url)
+		this.current++
+	} else {
+		this.current++
+		this.history[this.current] = url
+		this.history = this.history[:this.current+1]
+	}
 
 }
 
 func (this *BrowserHistory) Back(steps int) string {
+	if this.current-steps <= 0 {
+		this.current = 0
+	} else {
+		this.current -= steps
+	}
 
+	return this.history[this.current]
 }
 
 func (this *BrowserHistory) Forward(steps int) string {
-
+	if (steps + this.current) < len(this.history)-1 {
+		this.current += steps
+	} else {
+		this.current = len(this.history) - 1
+	}
+	return this.history[this.current]
 }
 
 /**
