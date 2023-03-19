90f1badabbb64d65d83640eb2e7b2d218c5ea7d6
tingyuchang <tingyugenius@gmail.com>
2023-03-19 10:49:50 +0800

    ADD desc

---
+++ data-structure/searchWords/searchWords.go
@@ -0,0 +1,23 @@
+package searchWords
+
+type WordDictionary struct {
+}
+
+func Constructor() WordDictionary {
+	return WordDictionary{}
+}
+
+func (this *WordDictionary) AddWord(word string) {
+
+}
+
+func (this *WordDictionary) Search(word string) bool {
+
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddWord(word);
+ * param_2 := obj.Search(word);
+ */
