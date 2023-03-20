d444337c7d2236a4cb5fb537fa92527e048140de
Matt Chang <tingyugenius@gmail.com>
2023-03-20 12:20:27 +0800

    ADD: 207 Course Schedule backtrack solution

--- Math/couseSchedule.go
+++ Math/couseSchedule.go
@@ -1,6 +1,44 @@
 package Math
 
 func CanFinish(numCourses int, prerequisites [][]int) bool {
+	cache := make(map[int][]int)
+	for i, _ := range prerequisites {
+		cache[prerequisites[i][1]] = append(cache[prerequisites[i][1]], prerequisites[i][0])
+	}
+
+	memo := make([]int, numCourses)
+
+	for i := 0; i < numCourses; i++ {
+		if !isCycle(&cache, &memo, i) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isCycle(cache *map[int][]int, memo *[]int, index int) bool {
+	if (*memo)[index] == 2 {
+		return false
+	}
+
+	if (*memo)[index] == 1 {
+		return true
+	}
+
+	(*memo)[index] = 2
+	for _, v := range (*cache)[index] {
+		if !isCycle(cache, memo, v) {
+			return false
+		}
+	}
+
+	(*memo)[index] = 1
+
+	return true
+}
+
+func CanFinish2(numCourses int, prerequisites [][]int) bool {
 	if len(prerequisites) == 0 {
 		return true
 	}
