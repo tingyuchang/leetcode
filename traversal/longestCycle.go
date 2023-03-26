7dd4c35b0511add88bafec16718cb8139f59fcf3
Matt Chang <tingyugenius@gmail.com>
2023-03-26 18:58:47 +0800

    ADD: 2360. Longest Cycle in a Graph

--- traversal/longestCycle.go
+++ traversal/longestCycle.go
@@ -2,13 +2,10 @@ package traversal
 
 func LongestCycle(edges []int) int {
 	res := -1
-	// -1: stop
-	// 0: not visited
-	// other: count
-	visited := make(map[int]int)
+
 	for i := 0; i < len(edges); i++ {
 
-		if visited[edges[i]] == 0 && edges[i] != -1 {
+		if edges[i] != -1 {
 			//fmt.Println("start", i)
 			count := 0
 			node := i
@@ -16,30 +13,22 @@ func LongestCycle(edges []int) int {
 			previous := -1
 			for {
 				//fmt.Println(previous, node)
-				if visited[node] > 0 {
-					//fmt.Println(isCycle, "pre: ", previous, "node: ", node)
-					//fmt.Println("visited ", visited)
-					//fmt.Println("count ", isCycle)
+				if isCycle[node] > 0 {
+					// fmt.Println("count ", isCycle)
 					count = isCycle[previous] - isCycle[node] + 1
-					for k, _ := range isCycle {
-						visited[k] = -1
-					}
+					break
+				}
+
+				if edges[node] == -1 {
+					count = 0
 					break
 				}
 
 				count++
 				isCycle[node] = count
-				visited[node] = count
 				previous = node
 				node = edges[node]
-				if node == -1 {
-					//fmt.Println("end", previous)
-					for k, _ := range isCycle {
-						visited[k] = -1
-					}
-					count = 0
-					break
-				}
+				edges[previous] = -1
 			}
 
 			if count > res && count != 0 {
@@ -47,6 +36,5 @@ func LongestCycle(edges []int) int {
 			}
 		}
 	}
-
 	return res
 }
