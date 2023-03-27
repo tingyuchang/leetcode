39d0de12b672048b7bad71fb565142b25a99412d
Matt Chang <tingyugenius@gmail.com>
2023-03-27 09:45:43 +0800

    ADD: Daily Practice 20230327

--- test/daily_test.go
+++ test/daily_test.go
@@ -4,26 +4,26 @@ import (
 	"fmt"
 	"github.com/magiconair/properties/assert"
 	_0230323 "leetcode/0_Daily_Prac/20230323"
-	_0230326 "leetcode/0_Daily_Prac/20230326"
+	_0230327 "leetcode/0_Daily_Prac/20230327"
 	"reflect"
 	"regexp"
 	"testing"
 )
 
-var name = _0230326.Name{}
-var mergeSort = _0230326.MergeSort
-var heapSort = _0230326.HeapSort
-var insertionSort = _0230326.InsertionSort
-var quickSort = _0230326.QuickSort
-var binarySearch = _0230326.BinarySearch
-var binarySearchRotated = _0230326.BinarySearchInRotatedArray
-var maxProduct = _0230326.MaxProduct
-var longestChar = _0230326.LongestCharatersInReplacement
-var minWindow = _0230326.MinWindow
-var nextPermutation = _0230326.NextPermutation
-var combination = _0230326.Combination
-var coinChange = _0230326.CoinChange
-var longestCommonSubsequence = _0230326.LongestCommonSubsequence
+var name = _0230327.Name{}
+var mergeSort = _0230327.MergeSort
+var heapSort = _0230327.HeapSort
+var insertionSort = _0230327.InsertionSort
+var quickSort = _0230327.QuickSort
+var binarySearch = _0230327.BinarySearch
+var binarySearchRotated = _0230327.BinarySearchInRotatedArray
+var maxProduct = _0230327.MaxProduct
+var longestChar = _0230327.LongestCharatersInReplacement
+var minWindow = _0230327.MinWindow
+var nextPermutation = _0230327.NextPermutation
+var combination = _0230327.Combination
+var coinChange = _0230327.CoinChange
+var longestCommonSubsequence = _0230327.LongestCommonSubsequence
 var medianFinder = _0230323.Constructor()
 
 func TestDaily(t *testing.T) {
