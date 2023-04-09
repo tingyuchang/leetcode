a8da37df6c5f74ffa349928b70b19ae12239ba87
Matt Chang <tingyugenius@gmail.com>
2023-04-09 21:58:00 +0800

    ADD: 1857 Largest Color

--- traversal/largestPath.go
+++ traversal/largestPath.go
@@ -6,6 +6,65 @@ func LargestPathValue(colors string, edges [][]int) int {
 func largestPathValue(colors string, edges [][]int) int {
 	res := 0
 
+	// source -> []destinations
+	cache := make(map[int][]int, 0)
+	dp := make([]map[byte]int, len(colors))
+	visited := make([]bool, len(colors))
+
+	for _, edge := range edges {
+		cache[edge[0]] = append(cache[edge[0]], edge[1])
+	}
+
+	for i := 0; i < len(colors); i++ {
+		if isCycle, temp := dfsLargestPathValue(i, cache, colors, visited, dp); isCycle {
+			return -1
+		} else {
+			for _, v := range temp {
+				if v > res {
+					res = v
+				}
+			}
+		}
+	}
 
 	return res
+}
+
+func dfsLargestPathValue(start int, cache map[int][]int, colors string, visited []bool, dp []map[byte]int) (bool, map[byte]int) {
+	if len(cache[start]) == 0 {
+		return false, map[byte]int{colors[start]: 1}
+	}
+
+	// dp?
+
+	if len(dp[start]) > 0 {
+		return false, dp[start]
+	}
+
+	visited[start] = true
+
+	res := make(map[byte]int)
+
+	for _, next := range cache[start] {
+		if visited[next] {
+			// cycle detected
+			return true, nil
+		}
+
+		isCycle, temp := dfsLargestPathValue(next, cache, colors, visited, dp)
+
+		if isCycle {
+			return true, nil
+		}
+		for k, v := range temp {
+			if v > res[k] {
+				res[k] = v
+			}
+		}
+	}
+
+	res[colors[start]]++
+	visited[start] = false
+	dp[start] = res
+	return false, res
 }
