fb391cf155db90be87a9d7ebd306a9f626b69a79
Matt Chang <tingyugenius@gmail.com>
2023-04-09 14:26:16 +0800

    ADD: test case & description

---
+++ traversal/largestPath.go
@@ -0,0 +1,11 @@
+package traversal
+
+func LargestPathValue(colors string, edges [][]int) int {
+	return largestPathValue(colors, edges)
+}
+func largestPathValue(colors string, edges [][]int) int {
+	res := 0
+
+
+	return res
+}
