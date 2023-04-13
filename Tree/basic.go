459e4c57ca52ea942847778cfc9f48f433594e73
Matt Chang <tingyugenius@gmail.com>
2023-04-13 10:16:00 +0800

    ADD: 700 Search in a Binary Search Tree

--- Tree/basic.go
+++ Tree/basic.go
@@ -1,5 +1,25 @@
 package Tree
 
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == val {
+		return root
+	}
+
+	if val < root.Val {
+		return searchBST(root.Left, val)
+	}
+
+	if val > root.Val {
+		return searchBST(root.Right, val)
+	}
+
+	return nil
+}
+
 func MinDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
