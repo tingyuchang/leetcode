84d878a2a6c86cfdd9abc77c18bd0018bb91616a
Matt Chang <tingyugenius@gmail.com>
2023-04-13 11:09:50 +0800

    ADD: 701. Insert into a Binary Search Tree

---
+++ Tree/insert.go
@@ -0,0 +1,30 @@
+package Tree
+
+// this problem is not difficult, but the interest part is tree height
+// the advanced condition can think about the tree balance
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	// recursive solution
+	if val < root.Val {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+
+	if val > root.Val {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+
+	// or for loop to find the insert node
+	/*
+		var node *treeNode
+		if val < node.Val => node = node.left
+		if val > node.Val => node = node.Right
+
+		until node == nil
+	*/
+
+	return root
+}
