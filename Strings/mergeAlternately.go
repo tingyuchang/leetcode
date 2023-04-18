0dfde6898ba9e5aee44886e7a6aba612136943dc
tingyuchang <tingyugenius@gmail.com>
2023-04-18 08:33:26 +0800

    ADD: 1768. Merge Strings Alternately

--- Strings/mergeAlternately.go
+++ Strings/mergeAlternately.go
@@ -1,1 +1,30 @@
 package Strings
+
+func mergeAlternately(word1 string, word2 string) string {
+	newWord := make([]byte, len(word1)+len(word2))
+	m, n, l := 0, 0, 0
+
+	for m < len(word1) && n < len(word2) {
+		newWord[l] = word1[m]
+		m++
+		l++
+		newWord[l] = word2[n]
+		n++
+		l++
+	}
+
+	for l < len(newWord) {
+		if m < len(word1) {
+			newWord[l] = word1[m]
+			m++
+		} else {
+			newWord[l] = word2[n]
+			n++
+		}
+
+		l++
+	}
+
+	return string(newWord)
+
+}
