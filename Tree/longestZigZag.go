80082c7a77195246d2a0765576f73d9ab3aff941
Matt Chang <tingyugenius@gmail.com>
2023-04-19 09:16:48 +0800

    ADD: 1372. Longest ZigZag Path in a Binary Tree

---
+++ Tree/longestZigZag.go
@@ -0,0 +1,38 @@
+package Tree
+
+func LongestZigZag(root *TreeNode) int {
+	return longestZigZag(root)
+}
+
+func longestZigZag(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	res := 0
+	zigZag(root.Left, false, &res, 0)
+	zigZag(root.Right, true, &res, 0)
+
+	return res
+}
+
+// direction true => right, false => left
+func zigZag(root *TreeNode, direction bool, res *int, currentLength int) {
+	if root == nil {
+		return
+	}
+
+	currentLength++
+
+	if currentLength > *res {
+		*res = currentLength
+	}
+
+	if direction {
+		zigZag(root.Left, false, res, currentLength)
+		zigZag(root.Right, true, res, 0)
+	} else {
+		zigZag(root.Left, false, res, 0)
+		zigZag(root.Right, true, res, currentLength)
+	}
+}
