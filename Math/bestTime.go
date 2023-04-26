8d3c6570f3e504eae8fc3d499f0bc754541a0aba
Matt Chang <tingyugenius@gmail.com>
2023-04-26 16:41:14 +0800

    ADD: 122. Best Time to Buy and Sell Stock II

--- Math/bestTime.go
+++ Math/bestTime.go
@@ -1,4 +1,16 @@
-package bestTimeToBuyAndSellStock
+package Math
+
+func MaxProfitII(prices []int) int {
+
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			profit += prices[i] - prices[i-1]
+		}
+	}
+
+	return profit
+}
 
 func MaxProfit2(prices []int) int {
 	minVal := prices[0]
--- test/math_test.go
+++ test/math_test.go
@@ -7,6 +7,24 @@ import (
 	"testing"
 )
 
+func TestBestTimeToBuyAndSell(t *testing.T) {
+	var testData = []struct {
+		input    []int
+		expected int
+	}{
+		{[]int{7, 1, 5, 3, 6, 4}, 5},
+		{[]int{7, 6, 4, 3, 1}, 0},
+		{[]int{1, 2}, 1},
+		{[]int{2, 4, 1}, 2},
+		{[]int{4, 7, 2, 1}, 3},
+	}
+
+	for _, tt := range testData {
+		result := Math.MaxProfit(tt.input)
+		assert.Equal(t, result, tt.expected)
+	}
+}
+
 func TestSmallestRangeII(t *testing.T) {
 	testData := []struct {
 		nums     []int
