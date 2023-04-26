1396e45bc12326a6d4bae7dd27139293353154c1
Matt Chang <tingyugenius@gmail.com>
2023-04-26 15:37:23 +0800

    UPD: add new approach for rotated array

--- Sort/rotateArray.go
+++ Sort/rotateArray.go
@@ -1,38 +1,59 @@
 package Sort
 
+import "fmt"
+
 func Rotate(nums []int, k int) {
 	rotate(nums, k)
 }
 
-func rotate(nums []int, k int) {
-	k = k % len(nums)
+/*
 
+approach 1  using queue/stack to push dada from end of array
+
+1,2,3,4,5,6,7 k = 3
+
+queue [7, 6, 5] -> pop element to the front of array
+
+TO (k+n)
+SO (k+n)
+
+approach 2 move elements in place
+
+k = k%len(nums)
+n = len(nums)
+ans[(i+k)%n] = nums[i]
+
+TO (n)
+SO (n)
+
+approach 3 reverse element
+
+reverse  array
+
+and reverse first k element and rest elements seperated
+
+
+*/
+
+func rotate(nums []int, k int) {
+
+	k = k % len(nums)
 	if k == 0 {
 		return
 	}
 
-	for i := 0; i < len(nums); i++ {
-
+	reverse := func(l, r int) {
+		for r > l {
+			nums[l], nums[r] = nums[r], nums[l]
+			l++
+			r--
+		}
 	}
 
+	reverse(0, len(nums)-1)
+	fmt.Println(nums)
+	reverse(0, k-1)
+	fmt.Println(nums)
+	reverse(k, len(nums)-1)
+	fmt.Println(nums)
 }
-
-//func rotate(nums []int, k int) {
-//
-//	k = k % len(nums)
-//
-//	if k == 0 {
-//		return
-//	}
-//
-//	temp := append([]int{}, nums[:len(nums)-k]...)
-//
-//	for i := 0; i < len(nums); i++ {
-//		if i < k {
-//			nums[i] = nums[len(nums)-k+i]
-//		} else {
-//			nums[i] = temp[i-k]
-//		}
-//	}
-//
-//}
