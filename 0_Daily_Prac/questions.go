700cbbc5c670913b32761ae10ef17cab26c2cc96
Matt Chang <tingyugenius@gmail.com>
2023-04-27 16:14:46 +0800

    ADD LIS

---
+++ 0_Daily_Prac/questions.go
@@ -0,0 +1,88 @@
+package __Daily_Prac
+
+func LengthOfLIS(nums []int) int {
+	return lengthOfLIS(nums)
+}
+
+func lengthOfLIS(nums []int) int {
+	/*
+		dp := make([]int, len(nums))
+			ans := 0
+			for i := 0; i < len(nums); i++ {
+				for j := 0; j < i; j++ {
+					if nums[i] > nums[j] && dp[i] <= dp[j] {
+						dp[i] = dp[j] + 1
+
+						if dp[i] > ans {
+							ans = dp[i]
+						}
+					}
+				}
+			}
+
+			return ans + 1
+	*/
+
+	subs := make([]int, 0)
+
+	for i := 0; i < len(nums); i++ {
+		if len(subs) == 0 || nums[i] > subs[len(subs)-1] {
+			subs = append(subs, nums[i])
+		} else {
+			l := 0
+			r := len(subs) - 1
+
+			replaceIndex := 0
+			for r >= l {
+				mid := int(uint(l+r) >> 1)
+				// fmt.Println(subs, nums[i], mid)
+				if mid > 0 && (subs[mid] == nums[i] || subs[mid] > nums[i] && subs[mid-1] < nums[i]) {
+					replaceIndex = mid
+					break
+				}
+
+				if subs[mid] > nums[i] {
+					r = mid - 1
+				} else {
+					l = mid + 1
+
+				}
+			}
+
+			subs[replaceIndex] = nums[i]
+
+		}
+
+	}
+
+	return len(subs)
+}
+
+/*
+
+Approach 1: DP
+TO(n^2)
+SO(n)
+nums = [10,9,2,5,3,7,101,18]
+
+     0 1 2 3 4 5 6 7
+10
+9    x
+2    x x
+5    x x 1
+3    x x 1 x
+7    x x 1 2 x
+101  1 x x 2 x 3
+18   1 x x 2 x 3
+
+Approach 2: Greedy & Binary Search
+
+holds a sub array
+
+if nums[i] > subArray[last] -> append it
+
+else find element which is the smallest element in elements which greater than nums[i]
+
+
+
+*/
