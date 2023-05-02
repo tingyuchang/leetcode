00e9bd86cba0d14953d198304a11f2871e671722
Matt Chang <tingyugenius@gmail.com>
2023-05-02 09:50:14 +0800

    ADD: 50. Pow(x, n)

--- Math/power.go
+++ Math/power.go
@@ -1,5 +1,42 @@
 package Math
 
+func myPow(x float64, n int) float64 {
+	if x == 0.0 {
+		return 0.0
+	}
+
+	if n == 0 || x == 1.0 {
+		return 1.0
+	}
+
+	if x == -1.0 {
+		if n%2 == 0 {
+			return 1
+		} else {
+			return -1
+		}
+	}
+
+	ans := 1.0
+	isNegative := false
+	if n < 0 {
+		n = -n
+		isNegative = true
+	}
+
+	// can improve by n/2
+	for n > 0 {
+		ans = ans * x
+		n--
+	}
+
+	if isNegative {
+		return 1 / ans
+	}
+
+	return ans
+}
+
 func IsPowerOfTwo(n int) bool {
 	if n <= 0 {
 		return false
