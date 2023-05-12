e378ee18771be0c55e16a9fcbecee6e724d6b2a7
Matt Chang <tingyugenius@gmail.com>
2023-05-12 08:37:00 +0800

    ADD: 57. Insert Interval

--- Math/interval.go
+++ Math/interval.go
@@ -1,92 +1,51 @@
 package Math
 
-import "fmt"
-
-func InsertInterval(intervals [][]int, newInterval []int) [][]int {
-	start := newInterval[0]
-	end := newInterval[1]
-
-	insertIndex := -1
-	endIndex := len(intervals)
-	merge := false
+func InsertInterval(intervals [][]int, newInterval []int) [][]int { // insert interval
+	intervals = insertInterval(intervals, newInterval)
+	ans := make([][]int, 0)
 	for i := 0; i < len(intervals); i++ {
-		interval := intervals[i]
-
-		if interval[0] <= start && interval[1] >= end {
-			return intervals
-		}
-		if interval[0] > end {
-			endIndex = i - 1
-			if insertIndex == -1 {
-				insertIndex = i - 1
-			}
-			break
+		currentInterval := intervals[i]
+		for i < len(intervals) && hasOverLap(currentInterval, intervals[i]) {
+			currentInterval = mergeIntervals(currentInterval, intervals[i])
+			i++
 		}
 
-		if interval[1] < start {
-			continue
-		}
-
-		if insertIndex == -1 {
-			insertIndex = i
-			merge = true
-		}
-
+		i--
+		ans = append(ans, currentInterval)
 	}
 
-	max := func(a, b int) int {
-		if a > b {
-			return a
-		}
+	return ans
 
-		return b
+}
+
+func insertInterval(intervals [][]int, newInterval []int) [][]int {
+	l := 0
+	r := len(intervals) - 1
+	for r >= l {
+
+		mid := int(uint(l+r) >> 1)
+
+		if intervals[mid][0] < newInterval[0] {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
 	}
 
-	min := func(a, b int) int {
-		if a > b {
-			return b
-		}
-		return a
-	}
-	fmt.Println(insertIndex, endIndex, merge)
-	// merge or insert?
-	if endIndex == len(intervals) {
-		if merge {
-			interval1 := intervals[insertIndex]
-			interval2 := intervals[endIndex-1]
-			start = min(start, interval1[0])
-			end = max(end, interval2[1])
-			intervals[insertIndex] = []int{start, end}
-			return intervals[:insertIndex+1]
-		}
-		return append(intervals, newInterval)
-	}
-
-	if insertIndex == -1 {
-		intervals = append([][]int{newInterval}, intervals...)
-		return intervals
-	}
-
-	if merge {
-		interval1 := intervals[insertIndex]
-		interval2 := intervals[endIndex]
-
-		start = min(start, interval1[0])
-		end = max(end, interval2[1])
-		intervals[insertIndex] = []int{start, end}
-		for i := insertIndex + 1; i <= insertIndex+len(intervals)-1-endIndex; i++ {
-			intervals[i] = intervals[endIndex-insertIndex+i]
-		}
-
-		return intervals[:insertIndex+len(intervals)-endIndex]
+	if l != len(intervals) {
+		intervals = append(intervals[:l+1], intervals[l:]...)
+		intervals[l] = newInterval
 	} else {
-		insertIndex++
-		intervals = append(intervals, []int{})
-
-		for i := len(intervals) - 1; i > insertIndex; i-- {
-			intervals[i] = intervals[i-1]
-		}
-		intervals[insertIndex] = []int{start, end}
-		return intervals
+		intervals = append(intervals, newInterval)
 	}
+
+	return intervals
+}
+
+func mergeIntervals(a, b []int) []int {
+	return []int{min(a[0], b[0]), max(a[1], b[1])}
+}
+
+func hasOverLap(a, b []int) bool {
+	return (min(a[1], b[1]) - max(a[0], b[0])) >= 0
 }
