71f806279dc3b680c24e0d4936cf78dd0a571c7d
mattc <tingyugenius@gmail.com>
2023-07-17 08:47:34 +0800

    ADD: Concurrency questions

--- Concurrency/printInOrder.go
+++ Concurrency/printInOrder.go
@@ -1,1 +1,84 @@
 package Concurrency
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Foo struct {
+	wg  sync.WaitGroup
+	wg1 sync.WaitGroup
+	wg2 sync.WaitGroup
+}
+
+func (f *Foo) first() {
+	defer f.wg.Done()
+	defer f.wg1.Done()
+	fmt.Println("first")
+}
+
+func (f *Foo) second() {
+	defer f.wg.Done()
+	defer f.wg2.Done()
+	f.wg1.Wait()
+	fmt.Println("second")
+}
+
+func (f *Foo) third() {
+	defer f.wg.Done()
+	f.wg2.Wait()
+	fmt.Println("third")
+}
+
+func (f *Foo) printFoo() {
+	defer f.wg.Done()
+	fmt.Println("Foo")
+	f.wg2.Done()
+}
+
+func (f *Foo) printBar() {
+	defer f.wg.Done()
+	f.wg2.Wait()
+	fmt.Println("Bar")
+}
+
+func (f *Foo) PrintInOrder(order []int) {
+
+	f.wg.Add(3)
+	f.wg1.Add(1)
+	f.wg2.Add(1)
+	if len(order) != 3 {
+		return
+	}
+	for _, v := range order {
+		switch v {
+
+		case 1:
+			go f.first()
+		case 2:
+			go f.second()
+		case 3:
+			go f.third()
+		default:
+			break
+		}
+	}
+
+	f.wg.Wait()
+}
+
+func (f *Foo) PrintFooBarAlternately(n int) {
+	for i := 0; i < n; i++ {
+		f.wg.Add(2)
+		f.wg2.Add(1)
+		go f.printFoo()
+		go f.printBar()
+		f.wg.Wait()
+	}
+
+}
+
+func TestFoo() {
+	foo := Foo{}
+	foo.PrintInOrder([]int{3, 2, 1})
+}
--- data-structure/treap/struct.go
+++ data-structure/treap/struct.go
@@ -1,1 +1,109 @@
 package treap
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type TreapNode struct {
+	Parent   *TreapNode
+	Left     *TreapNode
+	Right    *TreapNode
+	Val      int
+	Priority int
+}
+
+/*
+	 left
+
+				r                      R
+	          /   \                 /     \
+			L  	   R        ->     r      RR
+				/     \          /    \
+	           RL     RR       L       RL
+*/
+func (t *TreapNode) rotateLeft() *TreapNode {
+	right := t.Right
+	t.Right = right.Left
+	right.Left = t
+	return right
+}
+
+/*
+		 right
+
+					r                      L
+		          /   \                 /     \
+				L  	   R        ->     LL      r
+	          /   \          				/    \
+		    LL    LR       				   LR     R
+*/
+func (t *TreapNode) rotateRight() *TreapNode {
+	left := t.Left
+	t.Left = t.Left.Right
+	left.Right = t
+	return left
+}
+
+func newTreapNode(val int) *TreapNode {
+	return &TreapNode{
+		Parent:   nil,
+		Left:     nil,
+		Right:    nil,
+		Val:      val,
+		Priority: rand.Int() % 100,
+	}
+}
+
+func Insert(root *TreapNode, val int) *TreapNode {
+	if root == nil {
+		root = newTreapNode(val)
+		fmt.Printf("val: %d, priority: %d\n", root.Val, root.Priority)
+		return root
+	}
+
+	if val < root.Val {
+		root.Left = Insert(root.Left, val)
+
+		if root.Left != nil && root.Left.Priority < root.Priority {
+			fmt.Println("trigger right rotate ", root.Val)
+			root = root.rotateRight()
+		}
+	} else {
+		root.Right = Insert(root.Right, val)
+		if root.Right != nil && root.Right.Priority < root.Priority {
+			fmt.Println("trigger left rotate ", root.Val)
+			root = root.rotateLeft()
+		}
+	}
+
+	fmt.Println("-----")
+	PrintTreap(root, 0)
+	fmt.Println("----")
+	return root
+}
+
+func PrintTreap(root *TreapNode, space int) {
+	queue := []*TreapNode{root}
+
+	for len(queue) != 0 {
+		currentSize := len(queue)
+
+		for i := 0; i < currentSize; i++ {
+			currentNode := queue[i]
+			if currentNode == nil {
+				fmt.Printf("(nil)")
+				continue
+			}
+
+			fmt.Printf("%d(%d)", currentNode.Val, currentNode.Priority)
+			queue = append(queue, currentNode.Left)
+			queue = append(queue, currentNode.Right)
+		}
+
+		queue = queue[currentSize:]
+		fmt.Println()
+
+	}
+
+}
